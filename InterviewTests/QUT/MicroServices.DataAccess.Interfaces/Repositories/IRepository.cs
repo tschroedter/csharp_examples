d1e9df2947002690a03169d2d28b3f21f2e0a911
thomas <Thomas.Schroedter@gmx.net>
2017-05-25 11:34:17 +1000

    Added examples

---
+++ InterviewTests/QUT/MicroServices.DataAccess.Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+using MicroServices.DataAccess.Interfaces.Entities;
+
+namespace MicroServices.DataAccess.Interfaces.Repositories
+{
+    public interface IRepository <T>
+        where T : IEntity
+    {
+        IQueryable <T> All { get; }
+        T FindById(int id);
+        void Save(T instance);
+        void Remove(T entity);
+    }
+}
