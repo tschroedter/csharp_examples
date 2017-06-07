a9cf0215e1263d1628f6b22c0ac50ff4d2716203
thomas <Thomas.Schroedter@gmx.net>
2017-06-07 11:47:46 +1000

    KataPokerHand: Added example SpecFlow integration project

---
+++ Katas/KataPokerHand/KataPokerHand.Logic.Integration.Tests/Example/Calculator.cs
@@ -0,0 +1,15 @@
+﻿namespace KataPokerHand.Logic.Integration.Tests.Example
+{
+    public class Calculator
+    {
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+
+        public int Result { get; private set; }
+
+        public void Add()
+        {
+            Result = FirstNumber + SecondNumber;
+        }
+    }
+}
